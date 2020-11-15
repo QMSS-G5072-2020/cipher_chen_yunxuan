d5c35fb07e1566c0d6c69e88ef2ebdfb3119eeb0
yunxuan0724 <yc3682@columbia.edu>
2020-11-15 17:23:59 +0800

    add test

--- tests/test_cipher_yc3682.py
+++ tests/test_cipher_yc3682.py
@@ -3,3 +3,11 @@ from cipher_yc3682 import cipher_yc3682
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_cipher_1():
+    plain_text = 'this is a message.'
+    cipher_text = cipher_yc3682.cipher(plain_text, 5, True)
+    assert cipher_text == 'ymnx nx f rjxxflj.'
+    new_plain_text = cipher_yc3682.cipher(cipher_text, 5, False)
+    assert new_plain_text == 'this is a message.'
